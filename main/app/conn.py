9f779ef0308fb89760ab65c13862c02612567bcf
ali-rocco <ali-roccoto@gmail.com>
2022-12-09 18:01:08 +0600

    removing main directory

--- main/app/conn.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(
-    database="test_1_db",
-    host="db",
-    user="postgres",
-    password="postgres",
-    port="5432"
-        )
-cursor = conn.cursor()
